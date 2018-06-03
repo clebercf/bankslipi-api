0ef9113304c1108ed177e963204349ccf3c90b2b
Cleber Fonseca <clebercf@gmail.com>
2018-06-03 00:06:55 -0300

    Changed the verion of swagger doc just to check heroku deploy

--- src/main/java/br/com/contaazul/bankslip/api/controller/SwaggerConfig.java
+++ src/main/java/br/com/contaazul/bankslip/api/controller/SwaggerConfig.java
@@ -29,7 +29,7 @@ public class SwaggerConfig {
         ApiInfo apiInfo = new ApiInfo(
                 "Bankslip API",
                 "Spring Boot REST API for Bankslip",
-                "1.0",
+                "1.1",
                 "API to add, list, cancel and pay bank slips.",
                 new Contact("Cleber Fonseca", "", "clebercf@gmail.com"),
                "Private Use License",
